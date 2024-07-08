bb18490f579474c4f0f0bb54929739598e472b5a
yordan98 <penguinn2@mail.bg>
2024-07-08 20:00:46 +0300

    Initial commit

---
+++ src/main/java/com/management/userManagement/model/UserEntity.java
@@ -0,0 +1,83 @@
+package com.management.userManagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    @Size(max = 30)
+    private String firstName;
+
+    @Column(name = "last_name")
+    @Size(max = 30)
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Size(max = 15)
+    private String phoneNumber;
+
+    @Column(unique = true)
+    private String email;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
