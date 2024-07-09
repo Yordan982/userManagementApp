6c31bfaff401fa2778913c4cfd2ee96af503fc94
yordan98 <penguinn2@mail.bg>
2024-07-09 22:03:50 +0300

    Added update and delete user methods.

--- src/main/java/com/management/userManagement/controller/UserController.java
+++ src/main/java/com/management/userManagement/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.management.userManagement.controller;
 
-import org.springframework.ui.Model;
 import com.management.userManagement.dto.UserRegistrationDTO;
 import com.management.userManagement.model.UserEntity;
 import com.management.userManagement.repository.UserRepository;
 import com.management.userManagement.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller("/")
 public class UserController {
@@ -46,12 +46,54 @@ public class UserController {
         }
     }
 
+    @PutMapping("/user/{userId}/update")
+    public ResponseEntity<String> updateUser(@PathVariable Long userId, @Valid @RequestBody UserRegistrationDTO userRegistrationDTO, BindingResult bindingResult) {
+        try {
+            boolean isSuccess = userService.update(userId, userRegistrationDTO);
+
+            if (bindingResult.hasErrors()) {
+                List<String> errors = bindingResult.getAllErrors()
+                        .stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.toList());
+                return ResponseEntity.badRequest().body(String.join(", ", errors));
+            }
+
+            boolean isUpdated = userService.update(userId, userRegistrationDTO);
+            if (isUpdated) {
+                return ResponseEntity.ok("User updated successfully");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User id " + userId + " does not exist or the email is already taken.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+        }
+    }
+
     @GetMapping("/user/{id}")
     public ResponseEntity<String> viewUser(@PathVariable String id) {
         try {
             Long userId = Long.parseLong(id);
             Optional<UserEntity> user = userRepository.findById(userId);
             return user.map(userEntity -> ResponseEntity.status(HttpStatus.OK).body(userEntity.toString())).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("User id " + userId + " does not exist."));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid format for user id: " + id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+        }
+    }
+
+    @DeleteMapping("/user/{id}/delete")
+    public ResponseEntity<String> deleteUser(@PathVariable String id) {
+        try {
+            Long userId = Long.parseLong(id);
+            boolean isDeleted = userService.delete(userId);
+            if (isDeleted) {
+                userRepository.deleteById(Integer.valueOf(id));
+                return ResponseEntity.status(HttpStatus.OK).body("User id: " + userId + " was deleted.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User id " + userId + " does not exist.");
+            }
         } catch (NumberFormatException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid format for user id: " + id);
         } catch (Exception e) {
--- src/main/java/com/management/userManagement/service/UserService.java
+++ src/main/java/com/management/userManagement/service/UserService.java
@@ -7,8 +7,6 @@ import com.management.userManagement.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -29,7 +27,42 @@ public class UserService {
         }
     }
 
+    public boolean delete(Long id) {
+        if (isIdPresent(id)) {
+            userRepository.delete(userRepository.findById(id).get());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean update(Long id, UserRegistrationDTO user) {
+        if (isIdPresent(id) && isEmailTakenByCurrentUser(id, user)) {
+            UserEntity userEntity = userRepository.findById(id).get();
+            userEntity.setFirstName(user.getFirstName());
+            userEntity.setLastName(user.getLastName());
+            userEntity.setDateOfBirth(user.getDateOfBirth());
+            userEntity.setEmail(user.getEmail());
+            userEntity.setPhoneNumber(user.getPhoneNumber());
+            userEntity = userRepository.save(userEntity);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private boolean isEmailTaken(String email) {
         return userRepository.findByEmail(email).isPresent();
+    }
+
+    private boolean isEmailTakenByCurrentUser(Long id, UserRegistrationDTO user) {
+        if (isEmailTaken(user.getEmail())) {
+            return id.equals(userRepository.findByEmail(user.getEmail()).get().getId());
+        }
+        return false;
+    }
+
+    private boolean isIdPresent(Long id) {
+        return userRepository.findById(id).isPresent();
     }
 }
