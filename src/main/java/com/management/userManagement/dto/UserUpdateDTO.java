4c492c97e90c056b6e91b8fc5501e18179a0ff62
yordan98 <penguinn2@mail.bg>
2024-07-14 20:47:37 +0300

    Added a DTO class for updating a user.

---
+++ src/main/java/com/management/userManagement/dto/UserUpdateDTO.java
@@ -0,0 +1,69 @@
+package com.management.userManagement.dto;
+
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class UserUpdateDTO {
+    @NotEmpty(message = "First name is required")
+    @Size(min = 5, max = 30, message = "First name must be between 5 and 30 characters")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    @Size(min = 5, max = 30, message = "Last name must be between 5 and 30 characters")
+    private String lastName;
+
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be a past date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateOfBirth;
+
+    @NotEmpty(message = "Phone number is required")
+    @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 characters")
+    private String phoneNumber;
+
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
